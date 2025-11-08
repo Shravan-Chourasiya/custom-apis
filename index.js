b76d154708e7b1c93a946065f19398a31ecebc4b
Shravan Chourasiya <Shravan.r.chaursiya@gmail.com>
2025-11-08 15:52:48 +0000

    add cors fix

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const giftIdeas = require('./giftIdeasData.js');
 const app = express();
 const PORT = 3000;
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.get('/',(req,res)=>{
   const htmllist=`
